048307d3e4f697c9e83292e7fab3bb61aef885f2
MarwanHalabi <m.halvi@hotmail.com>
2025-04-17 13:09:49 +0300

    logging

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup(
     install_requires=[
         "requests",
         "flask",
-        "pymongo",
-        "pytz"
+        "pymongo"
     ],
     python_requires='>=3.8',
     entry_points={
         'console_scripts': [
-            'run-part_one = part_one.main:main',  # Entry point for part 1
+            'run-part1 = part_one.main:main',  # Entry point for part 1
             'run-part2 = part_two.main:main',  # Entry point for part 2
+            'run-part3 = part_two.main:main',  # Entry point for part 2
         ],
     },
 )
