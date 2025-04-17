a89402236bb4f9cd184d65525fdb75f96a6a70c3
MarwanHalabi <m.halvi@hotmail.com>
2025-04-17 11:59:41 +0300

    strcuture

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hired_score_package",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "flask",
+        "pymongo",
+        "pytz"
+    ],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'run-part1 = part_one.main:main',  # Entry point for part 1
+            'run-part2 = part_two.main:main',  # Entry point for part 2
+        ],
+    },
+)
